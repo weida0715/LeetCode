2c18031d03db656f9d4c40f4c2c2101785e2f759
weida0715 <106416298+weida0715@users.noreply.github.com>
2026-05-23 15:36:27 +0800

    Time: 59 ms (35.03%) | Space: 28.7 MB (42.85%) | Difficulty: Easy

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -3,12 +3,8 @@ class Solution:
         minSoFar = prices[0]
         ans = 0
 
-        for i in range(1, len(prices)):
-            profit = prices[i] - minSoFar
-            if profit > ans:
-                ans = profit
-
-            minSoFar = min(prices[i], minSoFar)
+        for price in prices:
+            ans = max(ans, price - minSoFar)
+            minSoFar = min(price, minSoFar)
 
         return ans
-        
