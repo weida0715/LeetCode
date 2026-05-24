4e8010042626bd83fced829510faa0aef0f37dc1
weida0715 <106416298+weida0715@users.noreply.github.com>
2026-05-24 23:55:58 +0800

    Time: 0 ms (100%) | Space: 23.9 MB (54.61%) | Difficulty: Easy

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int k = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                int temp = nums[k];
+                nums[k] = nums[i];
+                nums[i] = temp;
+                k++;
+            }
+        }
+    }
+};
