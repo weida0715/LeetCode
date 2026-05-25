e89973d10ea6b4d81b097fe2f34a2dd6a3a95f5d
weida0715 <106416298+weida0715@users.noreply.github.com>
2026-05-25 16:08:38 +0800

    Time: 0 ms (100%) | Space: 28.2 MB (27.25%) | Difficulty: Easy

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int mc = nums[0];
+        int count = 1;
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == mc) {
+                count++;
+            } else {
+                count--;
+                if (count == 0) {
+                    mc = nums[i];
+                    count = 1;
+                }
+            }
+        }
+
+        return mc;
+    }
+};
